74a647eed2378e69447f52dd442ce83a833390fb
Harry Man <harryman@Harrys-MacBook-Air-3.local>
2020-03-24 10:18:57 +0000

    First migration set up, both and up and down tested and working. Includes Exercise model and migration files.

---
+++ database/migrations/2020_03_24_085239_create_exercises_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExercisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exercises', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('exerciseName');
+            $table->string('exerciseDescription', 500);
+            $table->integer('exerciseRepetitions');
+            $table->string('exerciseImageURL');
+            $table->integer('exerciseDifficulty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exercises');
+    }
+}
