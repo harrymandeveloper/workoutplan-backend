1cc48e806fa3a692fab2f2ea9a06b449ca362276
Harry Man <harryman@Harrys-MacBook-Air-3.local>
2020-03-24 12:25:17 +0000

    migrations file added for creation of the days table

---
+++ database/migrations/2020_03_24_111010_create_days_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('days', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('dayName');
+            $table->integer('dayNumber');
+            $table->string('exercise1');
+            $table->string('exercise2');
+            $table->string('exercise3');
+            $table->string('exercise4');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('days');
+    }
+}
